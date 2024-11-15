ae8965308b07efda33a4577ad5ca5a9b4f023fa8
Heang Virith <virith.id@gmail.com>
2024-11-15 23:47:11 +0700

    Add authentication routes and views for login and registration

--- src/router/index.js
+++ src/router/index.js
@@ -1,23 +1,41 @@
 import { createRouter, createWebHistory } from 'vue-router'
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [
-    {
-      path: '/',
-      name: 'home',
-      component: () => import('../views/page/home/HomeView.vue')
-    },
-    {
-      path: '/about',
-      name: 'about',
-      component: () => import('../views/page/about/AboutView.vue')
-    },
-    {
-      path: '/contact',
-      name: 'contact',
-      component: () => import('../views/page/contact/ContactView.vue')
-    }
+	history: createWebHistory(import.meta.env.BASE_URL),
+	routes: [
+	{
+		path: '/',
+		name: 'home',
+		component: () => import('../views/page/home/HomeView.vue')
+	},
+	{
+		path: '/about',
+		name: 'about',
+		component: () => import('../views/page/about/AboutView.vue')
+	},
+	{
+		path: '/contact',
+		name: 'contact',
+		component: () => import('../views/page/contact/ContactView.vue')
+	},
+	{
+		path: '/auth',
+		component: { 
+			render: (c) => c('router-view') 
+		}, 
+		children: [
+			{
+				path: 'login',
+				name: 'login',
+				component: () => import('../views/page/authentication/LoginView.vue')
+			},
+			{
+				path: 'register',
+				name: 'register',
+				component: () => import('../views/page/authentication/RegisterView.vue')
+			}
+		]	
+	},
   ]
 })
 
