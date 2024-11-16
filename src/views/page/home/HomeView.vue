4d3bc50e312ddfbde001bcecc06aa26f96c1c813
Heang Virith <virith.id@gmail.com>
2024-11-16 20:57:08 +0700

    Add TheMiddleTopInvertedCard component and integrate it into HomeView

--- src/views/page/home/HomeView.vue
+++ src/views/page/home/HomeView.vue
@@ -45,6 +45,61 @@
             </v-row>
           </v-container>
         </v-row>
+        <v-row class="mb-16">
+            <v-container>
+                <h2 class="text-center font-bold text-gray-700 text-[2rem] mb-10">សម្ភារៈពេញនិយម</h2>
+                <v-row>
+                    <v-col cols="12" md="4">
+                        <a href="#">
+                            <the-short-bottom-right-inverted-card :price="79.99"></the-short-bottom-right-inverted-card>
+                        </a>
+                    </v-col>
+                    <v-col cols="12" md="4">
+                        <a href="#">
+                            <the-short-bottom-right-inverted-card href="lamp.png" :price="49.99" title="Edysse Lamps"></the-short-bottom-right-inverted-card>
+                        </a>
+                    </v-col>
+                    <v-col cols="12" md="4">
+                        <a href="#">
+                            <the-short-bottom-right-inverted-card href="small_chair.png" :price="89.99" title="ឈុតតុកៅអីក្មេង"></the-short-bottom-right-inverted-card>
+                        </a>
+                    </v-col>    
+                </v-row>
+            </v-container>
+        </v-row>
+        <v-row class="mb-12">
+            <v-container>
+                <v-row>
+                    <the-middle-top-inverted-card></the-middle-top-inverted-card>
+                </v-row>
+            </v-container>
+        </v-row>
+        <v-row>
+            <v-container>
+                <v-row>
+                    <v-col cols="12" md="6">
+                        <v-expansion-panels>
+                            <v-expansion-panel
+                                v-for="(expansionItem, i) in expansionItems"
+                                :key="i"
+                                class="text-[2rem]"
+                            >
+                                <v-expansion-panel-title class="px-6 py-6" :style="{ fontSize: '1.2rem' }">
+                                    {{ expansionItem.title }}
+                                </v-expansion-panel-title>
+                                <v-expansion-panel-text class="text-[1rem]">
+                                    {{ expansionItem.text }}
+                                </v-expansion-panel-text>
+                            </v-expansion-panel>
+                        </v-expansion-panels>
+                    </v-col>
+                    <v-spacer></v-spacer>
+                    <v-col cols="12" md="6">
+                        <v-img class="w-full h-full" src="desk.png" alt="Desk" />
+                    </v-col>
+                </v-row>
+            </v-container>
+        </v-row>
       </v-container>
     </div>
   </v-container>
@@ -58,6 +113,35 @@ import TheShortTopLeftInvertedCard from '@/components/cards/TheShortTopLeftInver
 import TheLongBottomRightInvertedCard from '@/components/cards/TheLongBottomRightInvertedCard.vue';
 import TheTopRightBottomLeftInvertedCard from '@/components/cards/TheTopRightBottomLeftInvertedCard.vue';
 import TheShortBottomLeftInvertedCard from '@/components/cards/TheShortBottomLeftInvertedCard.vue';
+import TheShortBottomRightInvertedCard from '@/components/cards/TheShortBottomRightInvertedCard.vue';
+import TheMiddleTopInvertedCard from '@/components/cards/TheMiddleTopInvertedCard.vue';
+
+const expansionItems = [
+    {
+        title: 'តើគ្រឿងសង្ហារឹមរបស់អ្នកនាំចូលពីប្រទេសណាខ្លះ?',
+        text: 'បាទ បងអាចអញ្ជើញមកហាងពួកយើងផ្ទាល់បាន ទីតាំងរបស់ពួកយើងគឺស្ថិតនៅលើផ្លូវ ២០០៤ រាជធានីភ្នំពេញ'
+    },
+    {
+        title: 'តើខ្ញុំអាចទៅមើលផ្ទាល់នៅហាងបានឬទេ?',
+        text: 'បាទ បងអាចអញ្ជើញមកហាងពួកយើងផ្ទាល់បាន ទីតាំងរបស់ពួកយើងគឺស្ថិតនៅលើផ្លូវ ២០០៤ រាជធានីភ្នំពេញ'
+    },
+    {
+        title: 'តើខ្ញុំអាចទៅមើលផ្ទាល់នៅហាងបានឬទេ?',
+        text: 'បាទ បងអាចអញ្ជើញមកហាងពួកយើងផ្ទាល់បាន ទីតាំងរបស់ពួកយើងគឺស្ថិតនៅលើផ្លូវ ២០០៤ រាជធានីភ្នំពេញ'
+    },
+    {
+        title: 'តើខ្ញុំអាចទៅមើលផ្ទាល់នៅហាងបានឬទេ?',
+        text: 'បាទ បងអាចអញ្ជើញមកហាងពួកយើងផ្ទាល់បាន ទីតាំងរបស់ពួកយើងគឺស្ថិតនៅលើផ្លូវ ២០០៤ រាជធានីភ្នំពេញ'
+    },
+    {
+        title: 'តើខ្ញុំអាចទៅមើលផ្ទាល់នៅហាងបានឬទេ?',
+        text: 'បាទ បងអាចអញ្ជើញមកហាងពួកយើងផ្ទាល់បាន ទីតាំងរបស់ពួកយើងគឺស្ថិតនៅលើផ្លូវ ២០០៤ រាជធានីភ្នំពេញ'
+    },
+    {
+        title: 'តើខ្ញុំអាចទៅមើលផ្ទាល់នៅហាងបានឬទេ?',
+        text: 'បាទ បងអាចអញ្ជើញមកហាងពួកយើងផ្ទាល់បាន ទីតាំងរបស់ពួកយើងគឺស្ថិតនៅលើផ្លូវ ២០០៤ រាជធានីភ្នំពេញ'
+    },
+];
 
 </script>
 
