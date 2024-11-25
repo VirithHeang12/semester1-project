09868cd6fc4f934cd8c5862e97afcc2d65b160cc
Heang Virith <virith.id@gmail.com>
2024-11-25 15:50:18 +0700

    Add Bootstrap for styling and update SoriyaView with input group

--- src/views/page/soriya/SoriyaView.vue
+++ src/views/page/soriya/SoriyaView.vue
@@ -1,3 +1,7 @@
 <template>
-    Soriya View
+    <div class="input-group mb-3">
+        <input type="text" class="form-control bg-gray-800" placeholder="Recipient's username"
+            aria-label="Recipient's username" aria-describedby="basic-addon2">
+        <span class="input-group-text" id="basic-addon2">@example.com</span>
+    </div>
 </template>
