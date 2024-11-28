885473dca8b16a6669aa16b482af9fbf373700ea
Rachna-An <anrachna11@gmail.com>
2024-11-29 00:38:59 +0700

    [rachna] modify TheCategoryCard, modify responsive LivingRoomView

--- src/views/page/livingroom/LivingRoomView.vue
+++ src/views/page/livingroom/LivingRoomView.vue
@@ -5,13 +5,7 @@
         <v-row class="mb-16" data-aos="fade-up">
           <v-col cols="12">
             <v-carousel hide-delimiters class="rounded-lg">
-              <v-carousel-item
-                v-for="(item, i) in carouselItems"
-                :key="`carousel-item-${i}`"
-                :src="item.src"
-                :lazy-src="item.lazySrc"
-                cover
-              ></v-carousel-item>
+              <v-carousel-item v-for="(item, i) in carouselItems" :key="`carousel-item-${i}`" :src="item.src" :lazy-src="item.lazySrc" cover></v-carousel-item>
             </v-carousel>
           </v-col>
         </v-row>
@@ -19,11 +13,7 @@
           <v-col cols="12" md="3" data-aos="fade-right">
             <v-list lines="one" select-strategy="leaf">
               <v-list-subheader>Product Category</v-list-subheader>
-              <v-list-item
-                v-for="item in sidebarItems"
-                :key="`sidebar-item-${item.id}`"
-                :title="item.title"
-              >
+              <v-list-item v-for="item in sidebarItems" :key="`sidebar-item-${item.id}`" :title="item.title">
                 <template v-slot:prepend="{ isSelected }">
                   <v-list-item-action start>
                     <v-checkbox-btn :model-value="isSelected"></v-checkbox-btn>
@@ -39,20 +29,9 @@
 
           <v-col cols="12" md="9">
             <v-row>
-              <v-col
-                cols="12"
-                md="4"
-                v-for="(card, index) in productCards"
-                :key="`product-card-${index}`"
-                data-aos="fade-up"
-                :data-aos-delay="index * 100"
-              >
-                <a href="#">
-                  <the-category-card
-                    :title="card.title"
-                    :price="card.price"
-                    :href="card.href"
-                  ></the-category-card>
+              <v-col cols="12" sm="6" md="4" v-for="(card, index) in productCards" :key="`product-card-${index}`" data-aos="fade-up" :data-aos-delay="index * 100">
+                <a href="#!">
+                  <the-category-card :title="card.title" :price="card.price" :href="card.href"></the-category-card>
                 </a>
               </v-col>
             </v-row>
@@ -63,14 +42,8 @@
     <v-sheet class="mx-auto" elevation="0" data-aos="fade-in">
       <h2 class="text-lg font-bold">អ្នកប្រហែលជាចូលចិត្ត</h2>
       <v-slide-group class="pa-4" selected-class="bg-success" show-arrows>
-        <v-slide-group-item
-          v-for="(cartrecom, n) in cartRecom"
-          :key="`cart-recom-${n}`"
-        >
-          <the-recommendation-card
-            :src="cartrecom.src"
-            :title="cartrecom.title"
-          ></the-recommendation-card>
+        <v-slide-group-item v-for="(cartrecom, n) in cartRecom" :key="`cart-recom-${n}`">
+          <the-recommendation-card :src="cartrecom.src" :title="cartrecom.title"></the-recommendation-card>
         </v-slide-group-item>
       </v-slide-group>
     </v-sheet>
